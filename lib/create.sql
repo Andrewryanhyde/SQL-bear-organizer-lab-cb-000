8d024fab7efc35f65895da5c00bfbdda5b08e24d
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-09 10:33:14 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,7 +2,7 @@ CREATE TABLE bears (
     id INTEGER PRIMARY KEY,
     name TEXT,
     age INTEGER,
-    gender char(1)("M"/"F"),
+    gender char(1),
     color TEXT,
     temperament TEXT,
     alive BOOLEAN
